f836085b8e9fd87573b3e9fc3130e992670efc7d
Jorge Acevedo <isc.acevedo@gmail.com>
2018-07-05 23:06:31 -0500

    Committing untracked files

--- app/src/main/java/com/example/jorgeacevedo/mooi/PostActivity.java
+++ app/src/main/java/com/example/jorgeacevedo/mooi/PostActivity.java
@@ -1,13 +1,196 @@
 package com.example.jorgeacevedo.mooi;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class PostActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+
+    private static final int Gallery_Pick = 1;
+
+    private Uri ImageUri;
+    private String Description;
+
+    private StorageReference PostImageReference;
+    private DatabaseReference UsersRef, PostRef;
+    private FirebaseAuth mAuth;
+
+    private String saveCurrentDate, saveCurrentTime, postRandomName, downloadUrl, current_user_id;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mAuth = FirebaseAuth.getInstance();
+        current_user_id = mAuth.getCurrentUser().getUid();
+
+
+        PostImageReference = FirebaseStorage.getInstance().getReference();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        PostRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+
+
+        SelectPostImage = (ImageButton) findViewById(R.id.select_post_image);
+        UpdatePostButton = (Button) findViewById(R.id.update_post_button);
+        PostDescription = (EditText) findViewById(R.id.post_description);
+
+
+        mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenGallery();
+            }
+        });
+
+        UpdatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ValidatePostInfo();
+            }
+        });
+    }
+
+    private void ValidatePostInfo() {
+        Description = PostDescription.getText().toString();
+        if (ImageUri == null){
+            Toast.makeText(this, "Please select an image...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(Description)){
+            Toast.makeText(this, "Please write your post...", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            StoringImageToFirebaseStorage();
+        }
+    }
+
+    private void StoringImageToFirebaseStorage() {
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        Calendar calForTime = Calendar.getInstance();
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH-mm");
+        saveCurrentTime = currentTime.format(calForTime.getTime());
+
+        postRandomName = saveCurrentDate + saveCurrentTime;
+
+        StorageReference filepath = PostImageReference.child("Post Images").child(ImageUri.getLastPathSegment() + postRandomName + ".jpg");
+        filepath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()){
+                    downloadUrl = task.getResult().getDownloadUrl().toString();
+                    Toast.makeText(PostActivity.this, "Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                    SavingPostInformationToDatabase();
+                }
+                else {
+                    String message = task.getException().getMessage();
+                    Toast.makeText(PostActivity.this, "Error occurred: " + message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void SavingPostInformationToDatabase() {
+        UsersRef.child(current_user_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    String userFullName = dataSnapshot.child("fullname").getValue().toString();
+                    String userProfileImage = dataSnapshot.child("profileimage").getValue().toString();
+
+                    HashMap postsMap = new HashMap();
+                    postsMap.put("uid", current_user_id);
+                    postsMap.put("date", saveCurrentDate);
+                    postsMap.put("time", saveCurrentTime);
+                    postsMap.put("description", Description);
+                    postsMap.put("postimage", downloadUrl);
+                    postsMap.put("profileimage", userProfileImage);
+                    postsMap.put("fullname", userFullName);
+
+                    PostRef.child(postsMap)
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, Gallery_Pick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==Gallery_Pick && resultCode==RESULT_OK && data != null){
+            ImageUri = data.getData();
+            SelectPostImage.setImageURI(ImageUri);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            SendUserToMainActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
